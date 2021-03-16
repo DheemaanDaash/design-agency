733997a7f2bd4dd8ca7562d1472c7e00aa228601
Dhiman Dash <53612989+DheemaanDaash@users.noreply.github.com>
2021-03-16 22:05:20 +0600

    Co-authored-by: AniketAgarwal21 <AniketAgarwal21@users.noreply.github.com>

--- app/models/menu.js
+++ app/models/menu.js
@@ -5,6 +5,7 @@ const menuSchema = new Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
+    description: { type: String, required: true },
     type: { type: String, required: true }
 })
 
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const expressLayout =  require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
 const mongoose = require('mongoose')
 const session = require('express-session')
+const flash = require('express-flash')
+// const MongoDbStore =  require ('connect-mongo')(session)
+const MongoStore = require('connect-mongo');
+
 
 //Database Connection
 const url = 'mongodb://localhost/design';
@@ -19,13 +23,20 @@ connection.once('open', () => {
     console.log('Connection failed...')
 });
 
-//Session Config
+//Session Config and Store
 app.use(session({
     secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge 1000 * 60 * 60 * 24} //24 hours
+    saveUninitialized: true,
+    resave: true,
+    store: MongoStore.create({
+        mongoUrl: "mongodb://localhost/design",
+        // db: mongoose.connection.db,
+        ttl: 5 * 24 * 60 * 60 // 5 days
+      }),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } //24 hours
 }))
+
+app.use(flash())
 
 //Asset
 app.use(express.static('public'))
